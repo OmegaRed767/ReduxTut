643c68df71c03a4d34ed7e5bdae83f01332b9e66
OmegaRed767 <omegared767@gmail.com>
2021-12-18 20:22:19 +0530

    push_from_2nd_b

---
+++ components/SecondBranch.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+const SecondBranch = () => {
+  return (
+    <View>
+      <Text>SecondBranch</Text>
+    </View>
+  );
+};
+export default SecondBranch;
