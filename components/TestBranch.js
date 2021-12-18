5a03905887fcba836effae176dbb4c960868767d
OmegaRed767 <omegared767@gmail.com>
2021-12-18 20:05:18 +0530

    branch-change

---
+++ components/TestBranch.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+const TestBranch = () => {
+  return (
+    <View>
+      <Text>TestBranch</Text>
+    </View>
+  );
+};
+export default TestBranch;
